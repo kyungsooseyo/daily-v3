80f598b6a6620b8560b3ceb80d3e14aaaf06bc75
wanglimin47 <wanglimin47@jd.com>
2023-07-05 17:13:41 +0800

    ✨ feat(vue): utilFN

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 // import './style.css'
 // import App from './App.vue'
-import App from './customRef.vue'
+// import App from './customRef.vue'
+import App from './utilsFn.vue'
 const app = createApp(App)
 app.mount('#app')
