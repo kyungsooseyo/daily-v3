0e088b7a3028ea317095659cb8ad6cf43d8ea1f9
wanglimin47 <wanglimin47@jd.com>
2023-07-06 11:16:59 +0800

    ✨ feat(vue): watch(complete)

--- src/watch.vue
+++ src/watch.vue
@@ -2,6 +2,9 @@
   <div>
     <button @click="obj.age++">change obj age</button>
     <button @click="person.age++">change person age</button>
+    <button @click="num++">change num</button>
+    <button @click="stopWatch">stop watch</button>
+    <h3>{{ num }}</h3>
   </div>
 </template>
 <script setup>
@@ -26,8 +29,27 @@ const person = reactive({
 // watch(()=>person.age,(newVal,oldVal)=>{
 //   console.log(newVal,oldVal)
 // })
-watch([num, () => person.age], (newVal, oldVal) => {
-  console.log(newVal, oldVal)
+// watch([num, () => person.age], (newVal, oldVal) => {
+//   console.log(newVal, oldVal)
+// },
+// {
+//   flush: 'post', //在下次DOM更新循环结束之后执行回调；如果设置成pre的话 通过ref 来获取dom的textContent textContent值是落后于数据的 
+// })
+// 监听回调
+// watch(num, (newVal, oldVal, onCleanUp) => {
+//   console.log('newVal', newVal);
+//   let timer = setInterval(() => {
+//     console.log(666);
+//   }, 1000);
+//   // 在组件卸载的时候执行onCleanUp
+//   onCleanUp(() => {
+//     // 清空回调里面的挂载的属性和方法
+//     clearInterval(timer)
+//   })
+// })
+//----- 停止监听
+const stopWatch = watch(num, (newVal, oldVal, onCleanUp) => {
+  console.log('newVal', newVal)
 })
 </script>
 
